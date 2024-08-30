78396a8bfe800a071e0c544006b126b9e033cc61
Gajanan-bhosale <109035757+Gajanan-bhosale@users.noreply.github.com>
2024-08-30 19:01:42 +0530

    path changed 1

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ const EmployeeModel = require('./modules/Employee')
 
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+    origin: ["https://bloggist-chi.vercel.app/"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials:true
+}))
 
 mongoose.connect('mongodb://127.0.0.1:27017/employee');
 
