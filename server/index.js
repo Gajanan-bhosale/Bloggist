d84d46045f0795160a1b969a5d3ac84819fe0035
Gajanan-bhosale <109035757+Gajanan-bhosale@users.noreply.github.com>
2024-09-04 10:19:19 +0530

    changes into frontend

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const app = express()
 app.use(express.json())
 app.use(cors(
     {
-        origin: ["https://bloggist-frontend.vercel.app"],
+        origin: ["https://majestic-marzipan-e50fd2.netlify.app"],
         methods: ["POST", "GET"],
         credentials: true
     }
