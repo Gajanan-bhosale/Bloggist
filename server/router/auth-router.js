7469a7bbe8ed2edcc7502056f69d7edbaabee674
Gajanan-bhosale <109035757+Gajanan-bhosale@users.noreply.github.com>
2024-09-10 12:53:46 +0530

    commit

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -16,69 +16,69 @@ router.get('/getUserData', (req, res) => {
 });
 
 router.post("/login",async (req, res) => {
-    const { email, password } = req.body;
-    EmployeeModel.findOne({ email: email })
-        .then(user => {
-            if (user) {
-                if (user.password === password) {
-                    // Return the user's name and email along with the success status
-                    res.json({ status: "success", message: "Login successful", user: { name: user.name, email: user.email } });
-                } else {
-                    res.json({ status: "incorrect_password", message: "The password is incorrect" });
-                }
-            } else {
-                res.json({ status: "user_not_found", message: "The user is not registered" });
-            }
-        })
+    // const { email, password } = req.body;
+    // EmployeeModel.findOne({ email: email })
+    //     .then(user => {
+    //         if (user) {
+    //             if (user.password === password) {
+    //                 // Return the user's name and email along with the success status
+    //                 res.json({ status: "success", message: "Login successful", user: { name: user.name, email: user.email } });
+    //             } else {
+    //                 res.json({ status: "incorrect_password", message: "The password is incorrect" });
+    //             }
+    //         } else {
+    //             res.json({ status: "user_not_found", message: "The user is not registered" });
+    //         }
+    //     })
     //     .catch(err => res.json({ status: "error", message: "An error occurred during login", error: err }));
-    // try {
-    //     const { email, password } = req.body;
+    try {
+        const { email, password } = req.body;
 
-    //     const userExist = await EmployeeModel.findOne({email });
-    //     console.log(userExist);
+        const userExist = await EmployeeModel.findOne({email });
+        console.log(userExist);
 
-    //     if(!userExist) {
-    //         return res.status(400).json({message: "Invalid Credentials"});
-    //     }
+        if(!userExist) {
+            return res.status(400).json({message: "Invalid Credentials"});
+        }
 
-    //     //const user = await bcrypt.comapre(password, userExist.password);
-    //     const user = await userExist.comparePassword(password);
+        //const user = await bcrypt.comapre(password, userExist.password);
+        const user = await userExist.comparePassword(password);
 
-    //     if(user) {
-    //         res.status(200).json({
-    //             msg: "login Succesful",
-    //             token: await userExist.generateToken(),
-    //             userId: userExist._id.toString(),
-    //         })
-    //     } else {
-    //         res.status(401).json({message: "Invalid email or Password"})
-    //     }
-    // } catch (error) {
-    //     res.status(500).json("Internal server error") 
-    // }
+        if(user) {
+            res.status(200).json({
+                msg: "login Succesful",
+                token: await userExist.generateToken(),
+                userId: userExist._id.toString(),
+            })
+        } else {
+            res.status(401).json({message: "Invalid email or Password"})
+        }
+    } catch (error) {
+        res.status(500).json("Internal server error") 
+    }
 });
 
 router.post('/register', async(req, res) => {
-    EmployeeModel.create(req.body)
-        .then(employees => res.json(employees))
-        .catch(err => res.json(err))
+    // EmployeeModel.create(req.body)
+    //     .then(employees => res.json(employees))
+    //     .catch(err => res.json(err))
 
-    // try {
-    //     const {name, email, password} = req.body;
-    //     const userExit = await UserActivation.findOne({email});
-    //     if (userExit) {
-    //         return res.status(400).json({ msg : "email already exist"})
-    //     }
+    try {
+        const {name, email, password} = req.body;
+        const userExit = await UserActivation.findOne({email});
+        if (userExit) {
+            return res.status(400).json({ msg : "email already exist"})
+        }
 
-    //     const saltRound = 10;
-    //     const hash_password = await bcrypt.hash(password, saltRound)
+        const saltRound = 10;
+        const hash_password = await bcrypt.hash(password, saltRound)
          
-    //     const userCreated = await EmployeeModel.create({name,email, password: hash_password});
+        const userCreated = await EmployeeModel.create({name,email, password: hash_password});
 
-    //     res.status(201).json({msg : userCreated, token: await userCreated.generateToken(), userId: userCreated._id >this.toString(),});
-    // } catch(error) {
-    //     res.status(500).json("internal server error")
-    // }
+        res.status(201).json({msg : userCreated, token: await userCreated.generateToken(), userId: userCreated._id >this.toString(),});
+    } catch(error) {
+        res.status(500).json("internal server error")
+    }
 
 
 })
