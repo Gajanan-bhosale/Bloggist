caa051c4db753438593124607c6859b43567ca25
Gajanan-bhosale <109035757+Gajanan-bhosale@users.noreply.github.com>
2024-09-10 13:18:06 +0530

    commit

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose")
-
 const URI = process.env.MONGODB_URI;
 mongoose.connect(URI)
 
 const connectDb = async () => {
     try {
         await mongoose.connect(URI);
-        console.log("Connection succesful")
+        console.log("Connection succesfulfor data base")
     }catch (error) {
         console.error("Database Connection failed")
         process.exit(0)
