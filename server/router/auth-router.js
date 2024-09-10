0ce3cbc97ae202b193b0abd1d18a4905ad683ce1
Gajanan-bhosale <109035757+Gajanan-bhosale@users.noreply.github.com>
2024-09-10 12:09:39 +0530

    commit

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,7 +1,7 @@
 const express = require("express")
 const router = express.Router();
 const EmployeeModel = require('../modules/Employee')
-const bcrypt = require("bcrypt")
+const bcrypt = require("bcryptjs")
 
 router.get('/getUserData', (req, res) => {
     EmployeeModel.find()
