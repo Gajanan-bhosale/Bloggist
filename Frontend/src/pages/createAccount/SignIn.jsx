2905d5106505ee5201bb069e1fa44c40dcb57e9c
Gajanan-bhosale <109035757+Gajanan-bhosale@users.noreply.github.com>
2024-09-10 12:13:35 +0530

    commit

--- Frontend/src/pages/createAccount/SignIn.jsx
+++ Frontend/src/pages/createAccount/SignIn.jsx
@@ -30,6 +30,7 @@ const SignIn = () => {
         headers: {
           "Content-Type": "application/json",
         },
+        credentials: 'include',
         body: JSON.stringify(user),
       });
       console.log("login Form", response);
