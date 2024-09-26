58f13f2b3cb0713674905d98cbb5eb56f8c83a06
Gajanan-bhosale <109035757+Gajanan-bhosale@users.noreply.github.com>
2024-09-26 13:57:59 +0530

    dashboard changes

--- server/index.js
+++ server/index.js
@@ -5,19 +5,6 @@ const authrouter = require("./src/router/auth-router")
 const postrouter = require("./src/router/post-route")
 const connectDb = require('./src/utils/db');
 const path = require('path')
-// const multer = require('multer')
-// const storage = multer.diskStorage({
-//     destination: function (req, file, cb) {
-//       cb(null, 'uploads')
-//     },
-//     filename: function (req, file, cb) {
-//       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-//       cb(null, file.fieldname + '-' + uniqueSuffix)
-//     }
-//   })
-
-//   const upload = multer({ storage: storage })
-
 
 const app = express()
 app.use(express.json())
