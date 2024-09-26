6352ba103fbb7f7436f57baa66cbcbce88de66ab
Gajanan-bhosale <109035757+Gajanan-bhosale@users.noreply.github.com>
2024-09-26 15:29:16 +0530

    index file changes

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ app.use(express.json())
 
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')))
 const corsOptions = {
-    origin: 'https://bloggist-frontend.vercel.app', 
+    origin: 'https://bloggist-frontend.vercel.app',
     methods: "GET, POST, PUT, DELETE, OPTIONS",
     credentials: true,
 }
